db011c71ec12b8b27a5f37dfdac726942f602ef0
Jake Lukawski <jakelukawski@me.com>
2020-10-23 21:30:01 -0500

    created main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+#use text files for recycle_tips
+class Recycle_tip:
+  def __init__():
+
+class Art_project:
+  def __init__(self,title,description,required_bags,required_candy_wrappers):
+    self.title = title
+    self.description = description
+    self.required_bags = required_bags
+    self.required_candy_wrappers = required_candy_wrappers
+  
+    
+#home page
+#should this be recycled?
+#notifies the user of how much plastic is used
+#stores the amount of plastic is used
+class New_recycle_entry:
+  def __init__():
+    
+  #should this be recycled?
+  def recyclable:
+    
+  def plastic_used:
+
+  
+  
+def main():
+  Art_project("Plastic Bag Dress","Dress made out of plastic grocery bags",100,0)
+  Art_project("Plastic pencil case","Pencil pouch made out of candy wrappers",0,100)
+  
+if __name__ == "__main__":
+  main()
+
+
