942ed5b6e400f67493f0fdf117dab4687fb43a88
linhdo39 <72121311+linhdo39@users.noreply.github.com>
2020-10-24 17:13:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,52 +3,68 @@
 #find a way to send notifications
 #instagram-style through swift
 #method to add tips
+#class Recycle_tip:
+#  def __init__():
 class Recycle_tip:
-  def __init__():
+  def __init__(self):
+    tips = []
+    f = open("Recycling_Tips.txt", "r")
+    line = f.readline()
+    while line:
+      tips.append(line)
+      line = f.readline()
+    f.close()
+    for tip in tips:
+        print(tip)
 
 import sqlite3
 conn = sqlite3.connect('database')
-conn.execute('''CREATE TABLE recyclable
-  (CREATE TABLE BOTTLE
-  (ID INT PRIMARY KEY NOT NULL,
-  Name TEXT NOT NULL,
-  Quantity INT NOT NULL,
-  Art CHAR(50));,
-  CREATE TABLE WRAPPER
-  (ID INT PRIMARY KEY NOT NULL,
-  Name TEXT NOT NULL,
-  Quantity INT NOT NULL,
-  Art CHAR(50));,
-  CREATE TABLE BUBBLE_WRAP
-  (ID INT PRIMARY KEY NOT NULL,
-  Name TEXT NOT NULL,
-  Quantity INT NOT NULL,
-  Art CHAR(50));)''')
-conn.execute("INSERT INTO BOTTLE(ID,Name,Quantity,Art)\
-  VALUES (1, 'bottle', 50, 'mushroom garden')");
-conn.execute("INSERT INTO WRAPPER(ID,Name,Quantity,Art) \
-  VALUES (2, 'small wrapper', 5, 'bracelet')");
-conn.execute("INSERT INTO WRAPPER(ID,Name,Quantity,Art) \
-  VALUES (3, 'medium wrapper', 2, 'corn purse' )");
-conn.execute("INSERT INTO BUBBLE_WRAP (ID,Name,Quantity,Art) \
-   VALUES (4, 'bubble wrap', 2, 'decorative cloud' )");
+conn.execute("PRAGMA foreign_keys = ON")
 
-print('Records recorded successfully')
-conn.commit()
-print('Total number of rows deleted:', conn.total_changes)
-cursor=conn.execute("SELECT Id, Name, Quantity, Art from recyclable")
-for row in cursor:
-  print('ID:', row[0])
-  print('Name:', row[1])
-  print('Quantity:', row[2])
-  print('Art:', row[3])
-print('opperations done successfully')
+conn.execute('''CREATE TABLE IF NOT EXISTS Customer
+  (ID INT PRIMARY KEY NOT NULL,
+  Name TEXT NOT NULL);''')
+print("Create Customer table")
+
+conn.execute("INSERT INTO Customer(ID,Name)\
+  VALUES (1, 'Abby')");
+conn.execute("INSERT INTO Customer(ID,Name)\
+  VALUES (2, 'Johnathan')");
+
+conn.execute('''CREATE TABLE Material(
+  RecyclingID INT PRIMARY KEY NOT NULL,
+  Type TEXT NOT NULL,
+  Average_weight REAL NOT NULL);''')
+print("Create Material table")
+
+conn.execute("INSERT INTO Material(RecyclingID,Type,Average_weight) \
+  VALUES (20, 'Plastic', 5)");
+
+conn.execute('''CREATE TABLE IF NOT EXISTS Shape(
+  ShapeID INT PRIMARY KEY NOT NULL,
+  Shape TEXT NOT NULL,
+  Average_weight REAL NOT NULL);''')
+print("Create Shape table")
+
+conn.execute("INSERT INTO Shape(ShapeID,Shape,Average_weight) \
+  VALUES (300, 'bottle', 5.5)");
+
+conn.execute('''CREATE TABLE IF NOT EXISTS Recycling_Customer(
+  RecyclingCustomerID INT PRIMARY KEY NOT NULL,
+  Customer_id INT,
+  Material_id INT,
+  Quantity INT NOT NULL,
+  Shape_id INT,
+  FOREIGN KEY(Customer_id) REFERENCES Customer(CustomerID),  
+  FOREIGN KEY(Material_id) REFERENCES Material(RecyclingID),
+  FOREIGN KEY(Shape_id) REFERENCES Shape(ShapeID));''')
 conn.close()
+ 
 #store amount of reusable materials
 #import new_recycle_entry to get the amount of reusable items
 #text file to store info
 #link the project ideas
-import New_recycle_entry
+#import New_recycle_entry
 class Art_project:
   def __init__(self,title,required_material):
     self.title = title
@@ -64,31 +80,29 @@ class Art_project:
     for i in range (len(project)):
       if title == project[i][1] and required_material >= int(project[i][0]):
         print(project[i][2])
-  
-    
+
+
 #home page
 #should this be recycled?
 #notifies the user of how much plastic is used
 #stores the amount of plastic is used
 #stores reusable materials
-class New_recycle_entry:
-  def __init__():
+#class New_recycle_entry:
+#  def __init__():
     
   #should this be recycled?
-  def recyclable:
+#  def recyclable:
     
-  def plastic_used:
+#  def plastic_used:
 
   
-import Recycle_tip
-import Art_project
-import New_recycle_entry
+#import Recycle_tip
+#import Art_project
+#import New_recycle_entry
 def main():
-  
-  Dress = Art_project("Plastic Bag Dress","Dress made out of plastic grocery bags",100,0)
-  Pencil_pouch = Art_project("Plastic pencil case","Pencil pouch made out of candy wrappers",0,100)
+  Recycle_tip()
+  Art_project("bottle",50)
+  Art_project("medium wrapper", 2)
   
 if __name__ == "__main__":
   main()
-
-
